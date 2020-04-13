7c8d5b4cce30e745f0ba1a007173dcf8acb124fd
yogeshmithoon66 <54835253+yogeshmithoon66@users.noreply.github.com>
2020-04-14 04:58:47 +0530

    Add files via upload

---
+++ assets/js/example.js
@@ -0,0 +1,26 @@
+$(window).scroll(function() {
+  var heroHeight = $('header').height();
+  var yPosition = $(document).scrollTop();
+  
+    
+  if (yPosition <= heroHeight) {
+    var effectFactor = yPosition / heroHeight;
+    var rotation = effectFactor * (Math.PI / 2 - Math.asin( (heroHeight - yPosition) / heroHeight ));
+    $('.hero').css({
+      '-webkit-transform': 'rotateX('+rotation+'rad)',
+      'transform': 'rotateX('+rotation+'rad)',
+    })
+    .find('.overlay').css('opacity', effectFactor);
+  }
+  
+  
+  /**
+   * Sticky nav-bar
+   */
+  if (yPosition <= heroHeight) {
+    $('nav').removeClass('fixed');
+  } else {
+    $('nav').addClass('fixed');
+  }
+  
+});
